28cbaa2e059c887575c316151effd4bfa48d85d7
daniel1997499 <daniel1997499@gmail.com>
2021-06-14 11:26:30 +0300

    added chart suggestions and reformat MVCController

--- src/main/java/com/example/demo/Controller/MVCController.java
+++ src/main/java/com/example/demo/Controller/MVCController.java
@@ -81,68 +81,6 @@ public class MVCController {
         return "redirect:/index";
     }
 
-    //SENSORDATA
-    @GetMapping("/allsensordata")
-    public String getAllSensorData(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
-        model.addAttribute("appContext", appContext);
-        model.addAttribute("allsensordatalist", sensorRepo.findAll());
-        return "allsensordata";
-    }
-
-    @GetMapping("/allsensordatachart")
-    public String showAllSensorDataChart(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
-        model.addAttribute("appContext", appContext);
-        List<SensorData> allSensorDataList = sensorRepo.findAll();
-
-        SimpleDateFormat yyyyMmDd = new SimpleDateFormat("yyyy-MM-dd"); //"yyyy-MM-dd HH:mm:ss"
-        String strDateNow = yyyyMmDd.format(new Date());
-        String strDateNowModifiedBeginning = strDateNow + " " + "00:00:00";
-        String strDateNowModifiedEnd = strDateNow + " " + "23:59:00";
-        Timestamp tsStartOfToday = Timestamp.valueOf(strDateNowModifiedBeginning);
-        Timestamp tsEndOfToday = Timestamp.valueOf(strDateNowModifiedEnd);
-
-        //TODO ADD METHOD TO REPOSITORY getBySensor OR SOMETHING LIKE THAT
-        List<String> chartData = allSensorDataList.stream()
-                .filter(x->x.getDataType().equalsIgnoreCase("temperature"))
-                .filter(x->x.getPosted().before(tsEndOfToday))
-                .filter(x->x.getPosted().after(tsStartOfToday))
-                .map(SensorData::getData).collect(Collectors.toList());
-
-        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
-        List<String> chartDates = allSensorDataList.stream()
-                .filter(x->x.getDataType().equalsIgnoreCase("temperature"))
-                .filter(x->x.getPosted().before(tsEndOfToday))
-                .filter(x->x.getPosted().after(tsStartOfToday))
-                .map(SensorData::getPosted)
-                .map(Timestamp::getTime)
-                .map(formatter::format)
-                .collect(Collectors.toList());
-        if(chartData.size() == chartDates.size()) {
-            model.addAttribute("chartDates", chartDates);
-            model.addAttribute("chartData", chartData);
-        }
-        else {
-            log.error("Chart data and chart dates lists differ in size");
-        }
-        return "allsensordatachart";
-    }
-
-    @GetMapping("/addsensordata")
-    public String addSensorData(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
-        model.addAttribute("appContext", appContext);
-        model.addAttribute("sensorData", new SensorData());
-        return "addsensordata";
-    }
-
-    @PostMapping("/savesensordata")
-    public String saveSensorData(@Valid @ModelAttribute SensorData sensorData, BindingResult result, Model model) {
-        if (result.hasErrors())
-            return "addsensordata";
-        if (sensorData.getDeviceId() != null && sensorData.getSensor() != null && sensorData.getDataType() != null && sensorData.getData() != null)
-            sensorRepo.save(sensorData);
-        return "redirect:/allsensordata";
-    }
-
     //DEVICES
     @GetMapping("/alldevices")
     public String getAllDevices(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
@@ -224,5 +162,105 @@ public class MVCController {
         if (device.getName() != null && device.getAddress() != null)
             devRepo.save(device);
         return "redirect:/alldevices";
+    }
+
+    //SENSORDATA
+    @GetMapping("/allsensordata")
+    public String getAllSensorData(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
+        model.addAttribute("appContext", appContext);
+        model.addAttribute("allsensordatalist", sensorRepo.findAll());
+        return "allsensordata";
+    }
+
+//    @PostMapping("/deletesensordata/id/{deviceID}") //TODO FINISH THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//    public String deleteSensorData(@PathVariable(value = "deviceID") Long deviceID, Model model) {
+//        devRepo.deleteById(deviceID);
+//        return "redirect:/alldevicespaginated";
+//    }
+
+    @GetMapping("/allsensordatachart")
+    public String showAllSensorDataChart(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
+        model.addAttribute("appContext", appContext);
+        List<SensorData> allSensorDataList = sensorRepo.findAll();
+
+        SimpleDateFormat yyyyMmDd = new SimpleDateFormat("yyyy-MM-dd"); //"yyyy-MM-dd HH:mm:ss"
+        String strDateNow = yyyyMmDd.format(new Date());
+        String strDateNowModifiedBeginning = strDateNow + " " + "00:00:00";
+        String strDateNowModifiedEnd = strDateNow + " " + "23:59:00";
+        Timestamp tsStartOfToday = Timestamp.valueOf(strDateNowModifiedBeginning);
+        Timestamp tsEndOfToday = Timestamp.valueOf(strDateNowModifiedEnd);
+
+        //TODO ADD METHOD TO REPOSITORY getBySensor OR SOMETHING LIKE THAT
+        List<String> chartData = allSensorDataList.stream()
+                .filter(x->x.getDataType().equalsIgnoreCase("temperature"))
+                .filter(x->x.getPosted().before(tsEndOfToday))
+                .filter(x->x.getPosted().after(tsStartOfToday))
+                .map(SensorData::getData).collect(Collectors.toList());
+        Integer totalSum = 0;
+        Integer counter = 0;
+        for (String str: chartData) {
+            Integer value = Integer.parseInt(str);
+            totalSum += value;
+            counter++;
+        }
+        Integer average = totalSum / counter;
+        String finalMessage = null;
+        String badgeToShow = null;
+        if (average < 0) {
+            finalMessage = "Too cold, or wrong sensor reading, try checking if device is working properly";
+            badgeToShow = "danger";
+        }
+        if (average > 0 && average < 20) {
+            finalMessage = "Average temperature is too cold: " + average + ", try raising the temperature to at least 20 degrees";
+            badgeToShow = "warning";
+        }
+        if (average > 19 && average < 25) {
+            finalMessage = "Average temperature: " + average + ", is optimal for working conditions";
+            badgeToShow = "success";
+        }
+        if (average > 24 && average < 40) {
+            finalMessage = "Average temperature is too high: " + average + " try lowering the temperature to t least 24 degrees";
+            badgeToShow = "warning";
+        }
+        if (average > 40) {
+            finalMessage = "Too hot, or wrong sensor reading, try checking if device is working properly";
+            badgeToShow = "darning";
+        }
+        model.addAttribute("tempMessage", finalMessage);
+        model.addAttribute("badgeToShow", badgeToShow);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
+        List<String> chartDates = allSensorDataList.stream()
+                .filter(x->x.getDataType().equalsIgnoreCase("temperature"))
+                .filter(x->x.getPosted().before(tsEndOfToday))
+                .filter(x->x.getPosted().after(tsStartOfToday))
+                .map(SensorData::getPosted)
+                .map(Timestamp::getTime)
+                .map(formatter::format)
+                .collect(Collectors.toList());
+        if(chartData.size() == chartDates.size()) {
+            model.addAttribute("chartDates", chartDates);
+            model.addAttribute("chartData", chartData);
+        }
+        else {
+            log.error("Chart data and chart dates lists differ in size");
+        }
+        return "allsensordatachart";
+    }
+
+    @GetMapping("/addsensordata")
+    public String addSensorData(@ModelAttribute("appContextInSession") AppContext appContext, Model model) {
+        model.addAttribute("appContext", appContext);
+        model.addAttribute("sensorData", new SensorData());
+        return "addsensordata";
+    }
+
+    @PostMapping("/savesensordata")
+    public String saveSensorData(@Valid @ModelAttribute SensorData sensorData, BindingResult result, Model model) {
+        if (result.hasErrors())
+            return "addsensordata";
+        if (sensorData.getDeviceId() != null && sensorData.getSensor() != null && sensorData.getDataType() != null && sensorData.getData() != null)
+            sensorRepo.save(sensorData);
+        return "redirect:/allsensordata";
     }
 }
